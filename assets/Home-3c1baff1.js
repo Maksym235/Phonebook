fb440d16566308aabe541ca93fdd0f1e383a172b
Maksym235 <Maksym235@users.noreply.github.com>
2023-07-18 13:33:51 +0000

    deploy: 1577cde2569f74fcb852ede30d717d152f406894

--- assets/Home-3c1baff1.js
+++ assets/Home-3c1baff1.js
@@ -1,4 +1,4 @@
-import{n as i,N as s,u as r,a as p,j as e,A as o}from"./index-3288e12c.js";const d=i.div`
+import{n as i,N as s,u as r,a as p,j as e,A as o}from"./index-49f4a552.js";const d=i.div`
   width: 100%;
   height: 100%;
   position: relative;
@@ -14,7 +14,7 @@ import{n as i,N as s,u as r,a as p,j as e,A as o}from"./index-3288e12c.js";const
   border-radius: 20px 0 0 20px;
   position: absolute;
   background-color: #bcbff2;
-  background-image: url("assets/mainMobile.png");
+  background-image: url("mainMobile.png");
   background-size: 289px 198px;
   background-repeat: no-repeat;
   background-position: right 40%;
@@ -32,7 +32,7 @@ import{n as i,N as s,u as r,a as p,j as e,A as o}from"./index-3288e12c.js";const
     height: 929px;
     border-radius: 50px 0px 0px 50px;
     background-color: #bcbff2;
-    background-image: url("assets/mainIimg.png");
+    background-image: url("mainIimg.png");
     background-size: 674px 661px;
     background-repeat: no-repeat;
     background-position: center center;
--- assets/Register-9d22381e.js
+++ assets/Register-9d22381e.js
@@ -1,4 +1,4 @@
-import{n as t,N as d,b as x,j as e,s as c}from"./index-3288e12c.js";const g=t.form`
+import{n,N as d,b as x,j as e,s as c}from"./index-49f4a552.js";const g=n.form`
   padding: 143px 0px;
   margin: 0px auto;
   display: flex;
@@ -7,7 +7,7 @@ import{n as t,N as d,b as x,j as e,s as c}from"./index-3288e12c.js";const g=t.fo
   justify-content: center;
   align-items: center;
   gap: 20px;
-`,m=t.h1`
+`,m=n.h1`
   margin: 0;
   margin-bottom: 30px;
   font-weight: 600;
@@ -15,7 +15,7 @@ import{n as t,N as d,b as x,j as e,s as c}from"./index-3288e12c.js";const g=t.fo
   line-height: 43px;
   letter-spacing: 0.01em;
   color: #1b1b1b;
-`,i=t.input`
+`,i=n.input`
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -36,7 +36,7 @@ import{n as t,N as d,b as x,j as e,s as c}from"./index-3288e12c.js";const g=t.fo
   @media (min-width: 768px) {
     width: 430px;
   }
-`,h=t.button`
+`,h=n.button`
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -55,13 +55,13 @@ import{n as t,N as d,b as x,j as e,s as c}from"./index-3288e12c.js";const g=t.fo
   @media (min-width: 768px) {
     width: 430px;
   }
-`,u=t.p`
+`,u=n.p`
   font-weight: 400;
   font-size: 17px;
   line-height: 26px;
   color: #1b1b1b;
   margin: 0;
-`,f=t(d)`
+`,f=n(d)`
   margin: 0;
   font-weight: 600;
   text-decoration: none;
@@ -70,11 +70,11 @@ import{n as t,N as d,b as x,j as e,s as c}from"./index-3288e12c.js";const g=t.fo
   display: flex;
   align-items: flex-end;
   color: #000000;
-`;function b(){const a=x(),r=o=>{o.preventDefault();const n=o.currentTarget,s=n.elements.name.value,p=n.elements.email.value,l=n.elements.password.value;a(c({name:s,email:p,password:l})),n.reset()};return e.jsxs(g,{onSubmit:r,children:[e.jsx(m,{children:"Sign Up"}),e.jsx(i,{label:"login",type:"text",name:"name",placeholder:"enter your login"}),e.jsx(i,{label:"email",type:"email",name:"email",placeholder:"enter your email"}),e.jsx(i,{label:"password",type:"password",name:"password",placeholder:"enter your password"}),e.jsx(h,{type:"submit",children:"Sign Up"}),e.jsx(u,{children:"Already have an account?"}),e.jsx(f,{to:"/login",children:"Log in"})]})}const w=t.div`
+`;function b(){const a=x(),r=o=>{o.preventDefault();const t=o.currentTarget,s=t.elements.name.value,p=t.elements.email.value,l=t.elements.password.value;a(c({name:s,email:p,password:l})),t.reset()};return e.jsxs(g,{onSubmit:r,children:[e.jsx(m,{children:"Sign Up"}),e.jsx(i,{label:"login",type:"text",name:"name",placeholder:"enter your login"}),e.jsx(i,{label:"email",type:"email",name:"email",placeholder:"enter your email"}),e.jsx(i,{label:"password",type:"password",name:"password",placeholder:"enter your password"}),e.jsx(h,{type:"submit",children:"Sign Up"}),e.jsx(u,{children:"Already have an account?"}),e.jsx(f,{to:"/login",children:"Log in"})]})}const w=n.div`
   width: 100%;
   margin-left: auto;
   margin-right: auto;
-  background-image: url("assets/FormBg.jpg");
+  background-image: url("FormBg.jpg");
   background-size: cover;
   border-radius: 50px;
 
